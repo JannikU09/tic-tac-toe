ede46198035210051a96ce9e79064f3c2dd71c24
JannikU09 <j.uphoff@innfactory.de>
2025-10-14 08:21:19 +0200

    style: Fehlendes Semikolon hinzugefügt.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Squares } from "./components/square/squares";
 import { Title } from "./components/title/title";
 import { Board } from "./components/board/board";
 import { Page } from "./page/page";
-import '@/app/page/page.css'
+import '@/app/page/page.css';
 
 export default function Home() {
   return (
