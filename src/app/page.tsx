b8d0fe8b65af5747d876096803b17dd8f0a2fc82
JannikU09 <j.uphoff@innfactory.de>
2025-10-16 10:08:41 +0200

    refactor: Unbenötigte Imports entfernt: - import Image from "next/image" - import styles from "./page.module.css" - import { Squares } from "./components/square/squares" - import { Title } from "./components/title/title" - import { Board } from "./components/board/board;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 "use client"
 
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Squares } from "./components/square/squares";
-import { Title } from "./components/title/title";
-import { Board } from "./components/board/board";
 import { Page } from "./page/page";
 import '@/app/page/page.css';
 
