824638f995632d3d20381964fc871e770d53a22c
JannikU09 <j.uphoff@innfactory.de>
2025-11-05 08:59:35 +0100

    feat: Hinzufügen der Option, ein "onCick" zubenutzen.

--- src/app/components/title/title.tsx
+++ src/app/components/title/title.tsx
@@ -1,12 +1,16 @@
 import '@/app/components/title/title.css'
 import React from 'react';
 
-export const Title = ({ title, size } : { title: string, size: string}) => {
+export const Title = ({ title, size, onClick }: { title: string, size: string, onClick?: () => void}) => {
     const cssClass = size === 'bigTitle' ? 'bigTitle' : 'smallTitle';
 
-    return(
-        <div className={`title ${cssClass}`}>
-            { title }
+    return (
+        <div 
+            className={`title ${cssClass}`}
+            onClick={onClick ? onClick: undefined}
+            style={{cursor: onClick ? 'pointer' : 'default'}}
+        >
+            {title}
         </div>
     );
 }
